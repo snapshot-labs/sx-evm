45f0abe73a7e5e6d593e95cbec3495ba88a8ebb9
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-12-19 17:59:02 +0000

    feat: added oz upgradeable submodule

--- src/interfaces/ISpace.sol
+++ src/interfaces/ISpace.sol
@@ -6,14 +6,12 @@ import "./space/ISpaceState.sol";
 import "./space/ISpaceActions.sol";
 import "./space/ISpaceOwnerActions.sol";
 import "./space/ISpaceEvents.sol";
-import "@zodiac/core/Module.sol";
 
 interface ISpace is 
     ISpaceState,
     ISpaceActions,
     ISpaceOwnerActions, 
-    ISpaceEvents, 
-    Module
+    ISpaceEvents
 {
 
 }
