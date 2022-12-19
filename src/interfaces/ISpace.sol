260559870db532c06a20369e7d05535889643127
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-12-19 16:38:05 +0000

    feat: sub interface categories

---
+++ src/interfaces/ISpace.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+import "./space/ISpaceState.sol";
+import "./space/ISpaceActions.sol";
+import "./space/ISpaceOwnerActions.sol";
+import "./space/ISpaceEvents.sol";
+
+interface ISpace is 
+    ISpaceState,
+    ISpaceActions,
+    ISpaceOwnerActions, 
+    ISpaceEvents
+
+{
+
+}
