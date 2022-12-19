b208abdd35d7194ebff3c99cf44698812443a199
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-12-19 17:39:17 +0000

    feat: added zodiac submodule

--- src/interfaces/ISpace.sol
+++ src/interfaces/ISpace.sol
@@ -6,14 +6,14 @@ import "./space/ISpaceState.sol";
 import "./space/ISpaceActions.sol";
 import "./space/ISpaceOwnerActions.sol";
 import "./space/ISpaceEvents.sol";
-
+import "@zodiac/core/Module.sol";
 
 interface ISpace is 
     ISpaceState,
     ISpaceActions,
     ISpaceOwnerActions, 
     ISpaceEvents, 
-    IModule
+    Module
 {
 
 }
