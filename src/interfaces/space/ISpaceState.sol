3ee872fffcf02ac07685eb23d46d251ce88aadb5
Orland0x <37511817+Orland0x@users.noreply.github.com>
2022-12-20 10:40:58 +0000

    feat: state access interface

--- src/interfaces/space/ISpaceState.sol
+++ src/interfaces/space/ISpaceState.sol
@@ -4,4 +4,8 @@ pragma solidity ^0.8.15;
 
 interface ISpaceState {
 
+    function hasVoted(uint256 proposalId, address voter) external view returns (bool);
+
+    function getProposalInfo(uint256 proposalId) external view returns (bool);
+
 }
