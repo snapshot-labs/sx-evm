37a75956a6dcd971b84f2fb1818a08bcd2d9330b
Scott Piriou <30843220+pscott@users.noreply.github.com>
2022-12-21 21:18:18 +0100

    Add basic proposal

--- src/Space.sol
+++ src/Space.sol
@@ -1,14 +1,172 @@
 // SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.13;
+import "forge-std/console2.sol";
 
 // Ownable? Eip712?
 contract Space {
-    uint256 private votingDelay;
-    uint256 private minVotingDuration;
-    uint256 private maxVotingDuration;
+    // notice: `uint32::max` corresponds to year ~2106.
+    struct Proposal {
+        uint256 quorum;
+        uint32 snapshotTimestamp;
+        uint32 startTimestamp;
+        uint32 minEndTimestamp;
+        uint32 maxEndTimestamp;
+        address executionStrategy;
+        bytes32 executionHash;
+    }
+
+    event ProposalCreated(
+        uint256 proposalId,
+        address proposerAddress,
+        Proposal proposal,
+        string metadataUri,
+        bytes executionParams
+    );
+
+    uint32 private votingDelay;
+    uint32 private minVotingDuration;
+    uint32 private maxVotingDuration;
 
     uint256 private proposalThreshold;
     uint256 private quorum;
-    mapping (address => bool) private executionStrategies;
-    address[] private votingStrategies;
+    mapping(address => bool) private executionStrategies;
+    mapping(address => bool) private authenticators;
+    mapping(address => bool) private votingStrategies;
+    mapping(address => bytes) private votingStrategiesParams;
+    uint256 private nextProposalNonce;
+    mapping(uint256 => Proposal) private proposalRegistry;
+    mapping(uint256 => bool) private executedProposals;
+
+    constructor(
+        uint32 _votingDelay,
+        uint32 _minVotingDuration,
+        uint32 _maxVotingDuration,
+        uint256 _proposalThreshold,
+        uint256 _quorum,
+        address[] memory _votingStrategies, // TODO: Add voting strategies params flat
+        bytes[] memory _votingStrategiesParams,
+        address[] memory _authenticators,
+        address[] memory _executionStrategies
+    ) {
+        require(_minVotingDuration <= _maxVotingDuration, "Min duration should be smaller than max duration");
+        require(_votingStrategies.length > 0, "Voting Strategies array empty");
+        require(_authenticators.length > 0, "Authenticators array empty");
+        require(_executionStrategies.length > 0, "Execution Strategies array empty");
+        require(
+            _votingStrategies.length == _votingStrategiesParams.length,
+            "Strategies and Strategies Parameters length mismatch"
+        );
+
+        votingDelay = _votingDelay;
+        minVotingDuration = _minVotingDuration;
+        maxVotingDuration = _maxVotingDuration;
+        proposalThreshold = _proposalThreshold;
+        quorum = _quorum;
+
+        for (uint i = 0; i < _votingStrategies.length; i++) {
+            address strategy = _votingStrategies[i];
+            votingStrategies[strategy] = true;
+            votingStrategiesParams[strategy] = _votingStrategiesParams[i];
+        }
+
+        for (uint i = 0; i < _executionStrategies.length; i++) {
+            executionStrategies[_executionStrategies[i]] = true;
+        }
+        for (uint i = 0; i < _authenticators.length; i++) {
+            authenticators[_authenticators[i]] = true;
+        }
+
+        nextProposalNonce = 1;
+    }
+
+    function _assertValidAuthenticator() private view {
+        require(authenticators[msg.sender], "Invalid Authenticator");
+    }
+
+    function _assertValidExecutionStrategy(address executionStrategy) private view {
+        require(executionStrategies[executionStrategy], "Invalid Execution Strategy");
+    }
+
+    // No way to declare a mapping in memory so we need to use an array and go for O(n^2)...
+    function _assertNoDuplicates(address[] memory arr) private pure {
+        if (arr.length > 0) {
+            for (uint i = 0; i < arr.length - 1; i++) {
+                for (uint j = i + 1; j < arr.length; j++) {
+                    require(arr[i] != arr[j], "Duplicates found"); // TODO: should we use a `if` to reduce gas cost?
+                }
+            }
+        }
+    }
+
+    function _getCumulativeVotingPower(
+        uint32 timestamp,
+        address userAddress,
+        address[] memory usedVotingStrategies,
+        bytes[] memory userVotingStrategyParams
+    ) private view returns (uint256) {
+        // Make sure there are no duplicates to avoid an attack where people double count a voting strategy
+        _assertNoDuplicates(usedVotingStrategies);
+
+        uint256 totalVotingPower = 0;
+        for (uint i = 0; i < usedVotingStrategies.length; i++) {
+            address strategyAddress = usedVotingStrategies[i];
+            bool strategyExists = votingStrategies[strategyAddress];
+            require(strategyExists, "Invalid used strategy");
+            // totalVotingPower += getVotingPower(timestamp, userAddress, votingStrategiesParams[strategyAddress],
+            // userVotingStrategyParams[strategyAddress]); // TODO: use SafeMath, check overflow and use Interface
+            totalVotingPower += 1; // TODO remove this
+        }
+
+        return totalVotingPower;
+    }
+
+    function propose(
+        address proposerAddress,
+        string calldata metadataUri,
+        address executionStrategy,
+        address[] calldata usedVotingStrategies,
+        bytes[] calldata userVotingStrategyParams,
+        bytes calldata executionParams
+    ) external {
+        _assertValidAuthenticator();
+        _assertValidExecutionStrategy(executionStrategy);
+        require(
+            usedVotingStrategies.length == userVotingStrategyParams.length,
+            "Used Strategies and Used Strategies Parameters length mismatch"
+        );
+
+        uint32 snapshotTimestamp = uint32(block.timestamp);
+
+        uint256 votingPower = _getCumulativeVotingPower(
+            snapshotTimestamp,
+            proposerAddress,
+            usedVotingStrategies,
+            userVotingStrategyParams
+        );
+        require(votingPower >= proposalThreshold, "Insufficient voting power");
+
+        // TODO: use SafeMath
+        uint32 startTimestamp = snapshotTimestamp + votingDelay;
+        uint32 minEndtimestamp = startTimestamp + minVotingDuration;
+        uint32 maxEndtimestamp = startTimestamp + maxVotingDuration;
+
+        // TODO: should we use encode or encodePacked?
+        bytes32 executionHash = keccak256(abi.encodePacked(executionParams));
+
+        // TODO: Is memory correct here?
+        Proposal memory proposal = Proposal(
+            quorum,
+            snapshotTimestamp,
+            startTimestamp,
+            minEndtimestamp,
+            maxEndtimestamp,
+            executionStrategy,
+            executionHash
+        );
+
+        proposalRegistry[nextProposalNonce] = proposal;
+        emit ProposalCreated(nextProposalNonce, proposerAddress, proposal, metadataUri, executionParams);
+
+        nextProposalNonce++;
+    }
 }
---
+++ test/Space.t.sol
@@ -0,0 +1,196 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.13;
+
+import "forge-std/Test.sol";
+import "../src/Space.sol";
+import "forge-std/console2.sol";
+
+contract SpaceTest is Test {
+    Space public space;
+    // address deployerAddress = ;// get address?
+    uint32 private votingDelay = 0;
+    uint32 private minVotingDuration = 0;
+    uint32 private maxVotingDuration = 1000;
+    uint32 private proposalThreshold = 1;
+    uint32 private quorum = 1;
+    address[] private votingStrategies = [address(0)];
+    bytes[] private votingStrategiesParams = [new bytes(0)];
+    address[] private authenticators = [address(this)];
+    address[] private executionStrategies = [address(0)];
+    bytes[] private userVotingStrategyParams = [new bytes(0)];
+    bytes private executionParams = new bytes(0);
+
+    string private metadataUri = "Snapshot On-Chain";
+
+    // TODO: add setters and test them (maybe in another test file)
+
+    function setUp() public {
+        space = new Space(
+            votingDelay,
+            minVotingDuration,
+            maxVotingDuration,
+            proposalThreshold,
+            quorum,
+            votingStrategies,
+            votingStrategiesParams,
+            authenticators,
+            executionStrategies
+        );
+    }
+
+    function testMinDurationSetUp() public {
+        vm.expectRevert("Min duration should be smaller than max duration");
+        new Space(
+            votingDelay,
+            maxVotingDuration + 1,
+            maxVotingDuration,
+            proposalThreshold,
+            quorum,
+            votingStrategies,
+            votingStrategiesParams,
+            authenticators,
+            executionStrategies
+        );
+   }
+
+    function testEmptyVotingStrategiesSetUp() public {
+        address[] memory emptyVotingStrategies = new address[](0);
+
+        vm.expectRevert("Voting Strategies array empty");
+        new Space(
+            votingDelay,
+            minVotingDuration,
+            maxVotingDuration,
+            proposalThreshold,
+            quorum,
+            emptyVotingStrategies,
+            votingStrategiesParams,
+            authenticators,
+            executionStrategies
+        );
+    }
+
+    function testEmptyAuthenticatorsSetUp() public {
+        address[] memory emptyAuthenticators = new address[](0);
+
+        vm.expectRevert("Authenticators array empty");
+        new Space(
+            votingDelay,
+            minVotingDuration,
+            maxVotingDuration,
+            proposalThreshold,
+            quorum,
+            votingStrategies,
+            votingStrategiesParams,
+            emptyAuthenticators,
+            executionStrategies
+        );
+    }
+
+    function testEmptyExecutionStrategiesSetUp() public {
+        address[] memory emptyExecutionStrategies = new address[](0);
+
+        vm.expectRevert("Execution Strategies array empty");
+        new Space(
+            votingDelay,
+            minVotingDuration,
+            maxVotingDuration,
+            proposalThreshold,
+            quorum,
+            votingStrategies,
+            votingStrategiesParams,
+            authenticators,
+            emptyExecutionStrategies
+        );
+    }
+
+    function testValidProposal() public {
+        // TODO: check that even got emitted
+        space.propose(
+            address(this),
+            metadataUri,
+            executionStrategies[0],
+            votingStrategies,
+            userVotingStrategyParams,
+            executionParams
+        );
+        // TODO: get proposal Info
+    }
+
+    function testInvalidAuth() public {
+        // Sender will not be a whiteslisted auth
+        vm.prank(address(0));
+
+        // Expect revert
+        vm.expectRevert("Invalid Authenticator");
+
+        space.propose(
+            address(this),
+            metadataUri,
+            executionStrategies[0],
+            votingStrategies,
+            userVotingStrategyParams,
+            executionParams
+        );
+    }
+
+    function testInvalidExecutionStrategy() public {
+        address invalidExecutionStrategy = address(1);
+
+        // Expect revert
+        vm.expectRevert("Invalid Execution Strategy");
+
+        space.propose(
+            address(this),
+            metadataUri,
+            invalidExecutionStrategy,
+            votingStrategies,
+            userVotingStrategyParams,
+            executionParams
+        );
+    }
+
+    function testInvalidUsedVotingStrategy() public {
+        address[] memory invalidUsedStrategy = new address[](1);
+        invalidUsedStrategy[0] = address(42);
+
+        // Expect revert
+        vm.expectRevert("Invalid used strategy");
+
+        space.propose(
+            address(this),
+            metadataUri,
+            executionStrategies[0],
+            invalidUsedStrategy,
+            userVotingStrategyParams,
+            executionParams
+        );
+    }
+
+    function testDuplicateUsedVotingStrategy() public {
+        address[] memory invalidUsedStrategy = new address[](4);
+        invalidUsedStrategy[0] = address(0);
+        invalidUsedStrategy[0] = address(1);
+        invalidUsedStrategy[0] = address(2);
+        invalidUsedStrategy[0] = address(0); // Duplicate entry
+
+        bytes[] memory userVotingStrategyParams2 = new bytes[](4);
+        userVotingStrategyParams2[0] = new bytes(0);
+        userVotingStrategyParams2[1] = new bytes(0);
+        userVotingStrategyParams2[2] = new bytes(0);
+        userVotingStrategyParams2[3] = new bytes(0);
+
+
+        // Expect revert
+        vm.expectRevert("Duplicates found");
+
+        space.propose(
+            address(this),
+            metadataUri,
+            executionStrategies[0],
+            invalidUsedStrategy,
+            userVotingStrategyParams2,
+            executionParams
+        );
+    }
+}
