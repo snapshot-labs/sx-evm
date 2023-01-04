db9fc2d663ac2d74b703c26983cb8c366cbff3ef
Scott Piriou <30843220+pscott@users.noreply.github.com>
2023-01-04 15:20:20 +0100

    Remove variable name for VanillaVotingStrategy and restrict mutability to pure

--- src/voting-strategies/VanillaVotingStrategy.sol
+++ src/voting-strategies/VanillaVotingStrategy.sol
@@ -5,13 +5,7 @@ pragma solidity ^0.8.15;
 import "../interfaces/IVotingStrategy.sol";
 
 contract VanillaVotingStrategy is IVotingStrategy {
-    // solhint-disable no-unused-vars
-    function getVotingPower(
-        uint256 timestamp,
-        address voterAddress,
-        bytes memory params,
-        bytes memory userParams
-    ) external view returns (uint256 votingPower) {
+    function getVotingPower(uint256, address, bytes memory, bytes memory) external pure returns (uint256 votingPower) {
         return 1;
     }
 }
