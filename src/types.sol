3630b0877c2b170ae7ed5d6bed74da9cb0db2bf4
Orlando <orlando@snapshot.org>
2023-01-12 18:36:31 +0000

    docs: comment explaining why we store data at proposal level

--- src/types.sol
+++ src/types.sol
@@ -3,9 +3,13 @@
 pragma solidity ^0.8.15;
 
 struct Proposal {
+    // We store the quroum for each proposal so that if the quorum is changed mid proposal,
+    // the proposal will still use the previous quorum *
     uint256 quorum;
     // notice: `uint32::max` corresponds to year ~2106.
     uint32 snapshotTimestamp;
+    // * The same logic applies for why we store the 3 timestamps below (which could otherwise
+    // be inferred from the votingDelay, minVotingDuration, and maxVotingDuration state variables)
     uint32 startTimestamp;
     uint32 minEndTimestamp;
     uint32 maxEndTimestamp;
