e2114784b66c6c9157b035aa9c611afe30ab3853
Orlando <orlando@snapshot.org>
2023-01-13 18:31:16 +0000

    feat: block timestamp to number resolver

---
+++ src/utils/TimestampResolver.sol
@@ -0,0 +1,22 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.15;
+
+abstract contract TimestampResolver {
+    error TimestampInFuture();
+    mapping(uint256 => uint256) public timestampToBlockNumber;
+    
+    function resolveSnapshotTimestamp(uint256 timestamp) external returns (uint256) {
+        if (timestamp > block.timestamp) revert TimestampInFuture();
+
+        uint256 blockNumber = timestampToBlockNumber[timestamp];
+        if (blockNumber != 0) {
+            // Timestamp already resolved, return the previously resolved block number
+            return blockNumber;
+        } 
+        // Timestamp not yet resolved, resolve it to the current block number and return it
+        timestampToBlockNumber[timestamp] = block.number;
+        return block.number;
+
+    }
+}
