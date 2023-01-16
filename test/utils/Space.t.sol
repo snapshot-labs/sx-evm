2a8f824758384aed680d6c0d17501bb3256b9e60
Orlando <orlando@snapshot.org>
2023-01-16 13:08:53 +0000

    fix: updated import paths

--- test/utils/Space.t.sol
+++ test/utils/Space.t.sol
@@ -5,10 +5,10 @@ pragma solidity ^0.8.15;
 import "forge-std/Test.sol";
 import { GasSnapshot } from "forge-gas-snapshot/GasSnapshot.sol";
 
-import "../src/Space.sol";
-import "../src/authenticators/VanillaAuthenticator.sol";
-import "../src/voting-strategies/VanillaVotingStrategy.sol";
-import "../src/execution-strategies/VanillaExecutionStrategy.sol";
+import "../../src/Space.sol";
+import "../../src/authenticators/VanillaAuthenticator.sol";
+import "../../src/voting-strategies/VanillaVotingStrategy.sol";
+import "../../src/execution-strategies/VanillaExecutionStrategy.sol";
 
 abstract contract SpaceTest is Test, GasSnapshot, ISpaceEvents, SpaceErrors {
     bytes4 constant PROPOSE_SELECTOR = bytes4(keccak256("propose(address,string,(address,bytes),(uint256,bytes)[])"));
