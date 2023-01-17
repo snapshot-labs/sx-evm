84876dbaae01d2796bf5713aed34ccffc79da24a
Orlando <orlando@snapshot.org>
2023-01-17 13:15:47 +0000

    style: comment out unused params

--- src/interfaces/IVotingStrategy.sol
+++ src/interfaces/IVotingStrategy.sol
@@ -2,7 +2,17 @@
 
 pragma solidity ^0.8.15;
 
+/// @title The interface for voting strategies
 interface IVotingStrategy {
+    /// @notice Get the voting power of an address at a given timestamp
+    /// @param timestamp The snapshot timestamp to get the voting power at
+    /// If a particular voting strategy requires a  block number instead of a timestamp,
+    /// the strategy should resolve the timestamp to a block number.
+    /// @param voterAddress The address to get the voting power of
+    /// @param params The global parameters that can configure the voting strategy for a particular space
+    /// @param userParams The user parameters that can be used in the voting strategy computation
+    /// @return votingPower The voting power of the address at the given timestamp
+    /// If there is no voting power, return 0.
     function getVotingPower(
         uint256 timestamp,
         address voterAddress,
