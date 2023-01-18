5037632e5218282746f329ee5e4240db46fe3003
Orlando <orlando@snapshot.org>
2023-01-18 13:13:13 +0000

    fix: use uint32 for timestampin Timestamp Resolver

--- src/utils/TimestampResolver.sol
+++ src/utils/TimestampResolver.sol
@@ -4,14 +4,17 @@ pragma solidity ^0.8.15;
 
 abstract contract TimestampResolver {
     error TimestampInFuture();
-    mapping(uint256 => uint256) public timestampToBlockNumber;
+    error InvalidBlockNumber();
+
+    mapping(uint32 => uint256) public timestampToBlockNumber;
 
     /// @notice Resolves a timestamp to a block number in such a way that the same timestamp
     /// always resolves to the same block number. If the timestamp is in the future, reverts.
     /// @param timestamp The timestamp to resolve
     /// @return blockNumber The block number that the timestamp resolves to
-    function resolveSnapshotTimestamp(uint256 timestamp) internal returns (uint256 blockNumber) {
-        if (timestamp > block.timestamp) revert TimestampInFuture();
+    function resolveSnapshotTimestamp(uint32 timestamp) internal returns (uint256 blockNumber) {
+        if (timestamp > uint32(block.timestamp)) revert TimestampInFuture();
+        if (block.number == 1) revert InvalidBlockNumber();
 
         blockNumber = timestampToBlockNumber[timestamp];
         if (blockNumber != 0) {
@@ -22,6 +25,7 @@ abstract contract TimestampResolver {
         // We resolve to the current block number - 1 so that Comp style getPastVotes/getPriorVotes
         // functions can be used in same block as when the resolution is made
         blockNumber = block.number - 1;
+
         timestampToBlockNumber[timestamp] = blockNumber;
         return blockNumber;
     }
