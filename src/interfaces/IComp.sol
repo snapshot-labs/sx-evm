976b6e370ce58d0ef5a6d9c11d70232ad643c0a6
Orlando <orlando@snapshot.org>
2023-01-18 14:14:38 +0000

    style: remove uint256  alias

--- src/interfaces/IComp.sol
+++ src/interfaces/IComp.sol
@@ -3,5 +3,5 @@
 pragma solidity ^0.8.15;
 
 interface IComp {
-    function getPriorVotes(address account, uint blockNumber) external view returns (uint96);
+    function getPriorVotes(address account, uint256 blockNumber) external view returns (uint96);
 }
