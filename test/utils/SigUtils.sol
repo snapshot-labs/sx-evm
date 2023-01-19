be0878567e343230dd40aba1ff732c48ff387e1a
Orlando <orlando@snapshot.org>
2023-01-19 15:27:27 +0000

    refactor: moved typed data hashing functions to SigUtils

--- test/utils/SigUtils.sol
+++ test/utils/SigUtils.sol
@@ -5,18 +5,22 @@ pragma solidity ^0.8.15;
 import "../../src/types.sol";
 
 abstract contract SigUtils {
-    bytes32 public constant DOMAIN_TYPEHASH =
+    string private name;
+    string private version;
+    bytes32 private constant DOMAIN_TYPEHASH =
         keccak256("EIP712Domain(string name,string version,uint256 chainId,address verifyingContract)");
-
-    bytes32 public constant STRATEGY_TYPEHASH = keccak256("Strategy(address addy,bytes params)");
-    bytes32 public constant INDEXED_STRATEGY_TYPEHASH = keccak256("IndexedStrategy(uint8 index,bytes params)");
-    bytes32 public constant PROPOSE_TYPEHASH =
+    bytes32 private constant STRATEGY_TYPEHASH = keccak256("Strategy(address addy,bytes params)");
+    bytes32 private constant INDEXED_STRATEGY_TYPEHASH = keccak256("IndexedStrategy(uint8 index,bytes params)");
+    bytes32 private constant PROPOSE_TYPEHASH =
         keccak256(
-            "Propose(address space,address author,string metadataUri,Strategy executionStrategy,IndexedStrategy[] userVotingStrategies,uint256 salt)"
+            "Propose(address space,address author,string metadataUri,Strategy executionStrategy,"
+            "IndexedStrategy[] userVotingStrategies,uint256 salt)"
         );
 
-    string private constant name = "SnapshotX";
-    string private constant version = "1";
+    constructor(string memory _name, string memory _version) {
+        name = _name;
+        version = _version;
+    }
 
     function _getProposeDigest(
         address authenticator,
@@ -31,13 +35,7 @@ abstract contract SigUtils {
             abi.encodePacked(
                 "\x19\x01",
                 keccak256(
-                    abi.encode(
-                        DOMAIN_TYPEHASH,
-                        _hashString(name),
-                        _hashString(version),
-                        block.chainid,
-                        authenticator
-                    )
+                    abi.encode(DOMAIN_TYPEHASH, _hashString(name), _hashString(version), block.chainid, authenticator)
                 ),
                 _hashPropose(space, author, metadataUri, executionStrategy, usedVotingStrategies, salt)
             )
@@ -50,11 +48,11 @@ abstract contract SigUtils {
         return keccak256(bytes(str));
     }
 
-    function _hashStrategy(Strategy memory strategy) internal view returns (bytes32) {
+    function _hashStrategy(Strategy memory strategy) internal pure returns (bytes32) {
         return keccak256(abi.encode(STRATEGY_TYPEHASH, strategy));
     }
 
-    function _hashIndexedStrategies(IndexedStrategy[] memory indexedStrategies) internal view returns (bytes32) {
+    function _hashIndexedStrategies(IndexedStrategy[] memory indexedStrategies) internal pure returns (bytes32) {
         bytes32[] memory indexedStrategyHashes = new bytes32[](indexedStrategies.length);
         for (uint256 i = 0; i < indexedStrategies.length; i++) {
             indexedStrategyHashes[i] = keccak256(abi.encode(INDEXED_STRATEGY_TYPEHASH, indexedStrategies[i]));
@@ -69,7 +67,7 @@ abstract contract SigUtils {
         Strategy memory executionStrategy,
         IndexedStrategy[] memory usedVotingStrategies,
         uint256 salt
-    ) internal view returns (bytes32) {
+    ) internal pure returns (bytes32) {
         return
             keccak256(
                 abi.encode(
