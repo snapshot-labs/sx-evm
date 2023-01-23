67742a86935f2ff3d031bdf654617c16c24bbdb5
Orlando <orlando@snapshot.org>
2023-01-23 15:51:00 +0000

    fix: replace executed boolean with counter

--- src/execution-strategies/VanillaExecutionStrategy.sol
+++ src/execution-strategies/VanillaExecutionStrategy.sol
@@ -5,15 +5,10 @@ pragma solidity ^0.8.15;
 import "../interfaces/IExecutionStrategy.sol";
 
 contract VanillaExecutionStrategy is IExecutionStrategy {
-    error AlreadyExecuted();
-    uint256 public executed;
+    uint256 numExecuted;
 
     // solhint-disable no-unused-vars
     function execute(ProposalOutcome proposalOutcome, bytes memory executionParams) external override {
-        if (executed == 0) {
-            executed = 1;
-        } else {
-            revert AlreadyExecuted();
-        }
+            numExecuted++;
     }
 }
