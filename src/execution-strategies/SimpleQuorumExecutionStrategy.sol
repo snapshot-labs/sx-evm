590c2d6b1225223d677d832ee5f7634451c52db9
Orlando <orlando@snapshot.org>
2023-02-14 12:34:45 +0000

    feat: simple quorum execution strategy base contract

--- src/execution-strategies/SimpleQuorumExecutionStrategy.sol
+++ src/execution-strategies/SimpleQuorumExecutionStrategy.sol
@@ -4,6 +4,16 @@ pragma solidity ^0.8.15;
 
 import "../interfaces/IExecutionStrategy.sol";
 
+// enum ProposalStatus {
+//     VotingDelay,
+//     VotingPeriod,
+//     VotingPeriodFinalizable,
+//     Finalizable,
+//     Executed,
+//     Rejected,
+//     Cancelled
+// }
+
 abstract contract SimpleQuorumExecutionStrategy is IExecutionStrategy {
     // solhint-disable no-unused-vars
     function execute(
@@ -16,35 +26,27 @@ abstract contract SimpleQuorumExecutionStrategy is IExecutionStrategy {
         uint256 votesFor,
         uint256 votesAgainst,
         uint256 votesAbstain
-    ) external view override returns (ProposalStatus) {
-        if (proposal.finalizationStatus == FinalizationStatus.NotExecuted) {
-            // Proposal has not been executed yet. Let's look at the current timestamp.
-            if (block.timestamp < proposal.startTimestamp) {
-                // Not started yet.
-                return ProposalStatus.WaitingForVotingPeriodToStart;
-            } else if (block.timestamp > proposal.maxEndTimestamp) {
-                // Voting period is over, this proposal is waiting to be finalized.
-                return ProposalStatus.Finalizable;
+    ) public view override returns (ProposalStatus) {
+        bool accepted = _quorumReached(proposal.quorum, votesFor, votesAgainst, votesAbstain) &&
+            _supported(votesFor, votesAgainst);
+        if (proposal.finalizationStatus == FinalizationStatus.Cancelled) {
+            return ProposalStatus.Cancelled;
+        } else if (proposal.finalizationStatus == FinalizationStatus.Executed) {
+            return ProposalStatus.Executed;
+        } else if (block.timestamp < proposal.startTimestamp) {
+            return ProposalStatus.VotingDelay;
+        } else if (block.timestamp < proposal.minEndTimestamp) {
+            return ProposalStatus.VotingPeriod;
+        } else if (block.timestamp < proposal.maxEndTimestamp) {
+            if (accepted) {
+                return ProposalStatus.VotingPeriodAccepted;
             } else {
-                // We are somewhere between `proposal.startTimestamp` and `proposal.maxEndTimestamp`.
-                if (block.timestamp > proposal.minEndTimestamp) {
-                    // We've passed `proposal.minEndTimestamp`, check if quorum has been reached.
-                    if (_quorumReached(proposal.quorum, votesFor, votesAgainst, votesAbstain)) {
-                        // Quorum has been reached, this proposal is finalizable.
-                        return ProposalStatus.VotingPeriodFinalizable;
-                    } else {
-                        // Quorum has not been reached so this proposal is NOT finalizable yet.
-                        return ProposalStatus.VotingPeriod;
-                    }
-                } else {
-                    // `proposal.minEndTimestamp` not reached, so we're just in the regular Voting Period.
-                    return ProposalStatus.VotingPeriod;
-                }
+                return ProposalStatus.VotingPeriod;
             }
+        } else if (accepted) {
+            return ProposalStatus.Accepted;
         } else {
-            // Proposal has been executed. Since `FinalizationStatus` and `ProposalStatus` only differ by
-            // one, we can safely cast it by substracting 1.
-            return ProposalStatus(uint8(proposal.finalizationStatus) - 1);
+            return ProposalStatus.Rejected;
         }
     }
 
@@ -56,5 +58,9 @@ abstract contract SimpleQuorumExecutionStrategy is IExecutionStrategy {
     ) internal pure returns (bool) {
         uint256 totalVotes = votesFor + votesAgainst + votesAbstain;
         return totalVotes >= quorum;
+    }
+
+    function _supported(uint256 votesFor, uint256 votesAgainst) internal pure returns (bool) {
+        return votesFor > votesAgainst;
     }
 }
