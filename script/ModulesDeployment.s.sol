cfc376499c6a6bb0da4f725e9e3fb286fa2beb21
Orland0x <37511817+Orland0x@users.noreply.github.com>
2023-02-15 11:13:18 -0500

    feat: deployment scripts (#34)
    * deployment: goerli vanilla space
    
    * chore: propose and vote script
    
    * Delete SpaceDeployment.t.sol
    
    * chore: added deployment scripts to config
    
    * feat: removed get address util from factory
    
    * feat: deployment scripts
    
    * refactor: formatting
    
    ---------
    
    Co-authored-by: Orlando <orlando@snapshot.org>

---
+++ script/ModulesDeployment.s.sol
@@ -0,0 +1,50 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.15;
+
+import "forge-std/Script.sol";
+
+import "../src/Space.sol";
+import "../src/authenticators/VanillaAuthenticator.sol";
+import "../src/voting-strategies/VanillaVotingStrategy.sol";
+import "../src/execution-strategies/VanillaExecutionStrategy.sol";
+
+import "../src/authenticators/EthSigAuthenticator.sol";
+import "../src/authenticators/EthTxAuthenticator.sol";
+
+import "../src/voting-strategies/CompVotingStrategy.sol";
+import "../src/voting-strategies/WhitelistStrategy.sol";
+
+import "../src/SpaceFactory.sol";
+
+contract ModulesDeployment is Script {
+    VanillaVotingStrategy public vanillaVotingStrategy;
+    CompVotingStrategy public compVotingStrategy;
+    WhitelistStrategy public whitelistStrategy;
+
+    VanillaAuthenticator public vanillaAuthenticator;
+    EthSigAuthenticator public ethSigAuthenticator;
+    EthTxAuthenticator public ethTxAuthenticator;
+
+    VanillaExecutionStrategy public vanillaExecutionStrategy;
+
+    SpaceFactory public spaceFactory;
+
+    function run() public {
+        uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
+        vm.startBroadcast(deployerPrivateKey);
+
+        vanillaVotingStrategy = new VanillaVotingStrategy();
+        compVotingStrategy = new CompVotingStrategy();
+        whitelistStrategy = new WhitelistStrategy();
+
+        vanillaAuthenticator = new VanillaAuthenticator();
+        ethSigAuthenticator = new EthSigAuthenticator("SOC", "0.1.0");
+        ethTxAuthenticator = new EthTxAuthenticator();
+
+        vanillaExecutionStrategy = new VanillaExecutionStrategy();
+
+        spaceFactory = new SpaceFactory();
+
+        vm.stopBroadcast();
+    }
+}
