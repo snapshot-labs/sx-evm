799337bb9e78490ce10ddb06c93e41951aa03776
arr00 <13561405+arr00@users.noreply.github.com>
2023-02-20 23:09:46 -0500

    remove vote salt

--- test/utils/SigUtils.sol
+++ test/utils/SigUtils.sol
@@ -77,8 +77,7 @@ abstract contract SigUtils {
         address voter,
         uint256 proposalId,
         Choice choice,
-        IndexedStrategy[] memory usedVotingStrategies,
-        uint256 salt
+        IndexedStrategy[] memory usedVotingStrategies
     ) internal view returns (bytes32) {
         bytes32 digest = keccak256(
             abi.encodePacked(
@@ -93,7 +92,7 @@ abstract contract SigUtils {
                     )
                 ),
                 keccak256(
-                    abi.encode(VOTE_TYPEHASH, space, voter, proposalId, choice, usedVotingStrategies.hash(), salt)
+                    abi.encode(VOTE_TYPEHASH, space, voter, proposalId, choice, usedVotingStrategies.hash())
                 )
             )
         );
