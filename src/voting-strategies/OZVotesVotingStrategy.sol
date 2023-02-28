7ee3c1e4a2e829021d4ce8a5ab9eb414e749691d
arr00 <13561405+arr00@users.noreply.github.com>
2023-02-28 14:51:31 -0500

    rebase and update sol version

--- src/voting-strategies/OZVotesVotingStrategy.sol
+++ src/voting-strategies/OZVotesVotingStrategy.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.18;
 
 import "../interfaces/IVotingStrategy.sol";
 import "@openzeppelin/contracts/governance/utils/IVotes.sol";
--- test/mocks/ERC20VotesToken.sol
+++ test/mocks/ERC20VotesToken.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.15;
+pragma solidity ^0.8.18;
 
 import { ERC20Votes, ERC20, ERC20Permit } from "@openzeppelin/contracts/token/ERC20/extensions/ERC20Votes.sol";
 
