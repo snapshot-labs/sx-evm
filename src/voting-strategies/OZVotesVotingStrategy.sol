000c4a584e7489031a4d06659859a64171a16a9e
Arr00 <13561405+arr00@users.noreply.github.com>
2023-03-01 05:32:27 -0500

    feat: Add OZVotesVotingStrategy (#91)
    * feat: Add OZ ERC20VotesStrategy
    
    * lint
    
    * format
    
    * Rename `ERC20VotingStrategy` to `OZVotesVotingStrategy`
    
    * feat: Add OZ ERC20VotesStrategy
    
    * lint
    
    * format
    
    * Rename `ERC20VotingStrategy` to `OZVotesVotingStrategy`
    
    * rebase and update sol version

---
+++ src/voting-strategies/OZVotesVotingStrategy.sol
@@ -0,0 +1,37 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import "../interfaces/IVotingStrategy.sol";
+import "@openzeppelin/contracts/governance/utils/IVotes.sol";
+import "../utils/TimestampResolver.sol";
+
+contract OZVotesVotingStrategy is IVotingStrategy, TimestampResolver {
+    error InvalidByteArray();
+
+    function getVotingPower(
+        uint32 timestamp,
+        address voterAddress,
+        bytes calldata params,
+        bytes calldata /* userParams */
+    ) external override returns (uint256) {
+        address tokenAddress = BytesToAddress(params, 0);
+        uint256 blockNumber = resolveSnapshotTimestamp(timestamp);
+        return uint256(IVotes(tokenAddress).getPastVotes(voterAddress, blockNumber));
+    }
+
+    /// @notice Extracts an address from a byte array
+    /// @param _bytes The byte array to extract the address from
+    /// @param _start The index to start extracting the address from
+    /// @dev Function from the library, with the require switched for a revert statement:
+    /// https://github.com/GNSPS/solidity-bytes-utils/blob/master/contracts/BytesLib.sol
+    function BytesToAddress(bytes memory _bytes, uint256 _start) internal pure returns (address) {
+        if (_bytes.length < _start + 20) revert InvalidByteArray();
+        address tempAddress;
+
+        assembly {
+            tempAddress := div(mload(add(add(_bytes, 0x20), _start)), 0x1000000000000000000000000)
+        }
+
+        return tempAddress;
+    }
+}
