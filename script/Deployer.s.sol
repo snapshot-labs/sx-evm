4233a25fd8bd0a781d616da27ef14f399fd48f41
Orlando <orlando@snapshot.org>
2023-03-09 20:34:29 +0000

    chore: formatting

--- script/Deployer.s.sol
+++ script/Deployer.s.sol
@@ -2,18 +2,35 @@
 pragma solidity ^0.8.18;
 
 import { Script } from "forge-std/Script.sol";
-
+import { stdJson } from "forge-std/StdJson.sol";
+import { Space } from "../src/Space.sol";
+import { VanillaAuthenticator } from "../src/authenticators/VanillaAuthenticator.sol";
+import { VanillaVotingStrategy } from "../src/voting-strategies/VanillaVotingStrategy.sol";
+import { VanillaExecutionStrategy } from "../src/execution-strategies/VanillaExecutionStrategy.sol";
+import { AvatarExecutionStrategy } from "../src/execution-strategies/AvatarExecutionStrategy.sol";
+import { EthSigAuthenticator } from "../src/authenticators/EthSigAuthenticator.sol";
 import { EthTxAuthenticator } from "../src/authenticators/EthTxAuthenticator.sol";
+import { CompVotingStrategy } from "../src/voting-strategies/CompVotingStrategy.sol";
+import { WhitelistStrategy } from "../src/voting-strategies/WhitelistStrategy.sol";
+import { ProxyFactory } from "../src/ProxyFactory.sol";
 
 interface SingletonFactory {
     function deploy(bytes memory _initCode, bytes32 salt) external returns (address payable);
 }
 
 contract Deployer is Script {
+    SingletonFactory internal singletonFactory = SingletonFactory(0xce0042B868300000d44A59004Da54A005ffdcf9f);
     address internal deployer;
     string internal deployments;
     string internal deploymentsPath;
-    bytes32 internal salt = bytes32(0); //keccak256(abi.encodePacked("SX DEPLOYER SALT 0"));
+
+    string internal name = "snapshot-x";
+    string internal version = "0.1.0";
+
+    using stdJson for string;
+
+    // Salt used for all singleton deployments
+    bytes32 internal salt = keccak256(abi.encodePacked("SX_DEPLOYER_SALT"));
 
     function run() public {
         uint256 pvk = vm.envUint("PRIVATE_KEY");
@@ -22,29 +39,49 @@ contract Deployer is Script {
         string memory network = vm.envString("NETWORK");
         uint32 chainId = getChainId(network);
 
-        bool redeploy = false;
-        try vm.envBool("REDEPLOY") returns (bool a) {
-            redeploy = a;
-        } catch {}
+        deploymentsPath = string.concat(string.concat("./deployments/", network), ".json");
 
-        string memory dep_loc = "./deployments/";
-        string memory dep_path = string.concat(dep_loc, network);
-        deploymentsPath = string.concat(dep_path, ".json");
-        if (!redeploy) {
-            try vm.readFile(deploymentsPath) returns (string memory prev_dep) {
-                deployments = prev_dep;
-            } catch {
-                // create the file
-                vm.writeFile(deploymentsPath, "{}");
-            }
-        } else {
-            // create the file if it doesnt exist, overwrite it if it does
-            vm.writeFile(deploymentsPath, "{}");
-        }
-        vm.startBroadcast(pvk);
-        SingletonFactory singletonFactory = SingletonFactory(0xce0042B868300000d44A59004Da54A005ffdcf9f);
-        address addy = singletonFactory.deploy(abi.encodePacked(type(EthTxAuthenticator).creationCode), salt);
-        // (address ps, ) = noRedeploy(type(VanillaAuthenticator).creationCode, "VanillaAuthenticator");
+        vm.startBroadcast(deployer);
+
+        string memory json = "deploymentArtifact";
+        (address vanillaExecutionStrategy, ) = noRedeploy(type(VanillaExecutionStrategy).creationCode);
+        json.serialize("VanillaExecutionStrategy", vanillaExecutionStrategy);
+
+        (address avatarExecutionStrategy, ) = noRedeploy(type(AvatarExecutionStrategy).creationCode);
+        // TODO: initialize avatar implementation
+        // AvatarExecutionStrategy(avatarExecutionStrategy).
+        // setUp(abi.encode(address(0x1), address(0x1), new address[](0)));
+        json.serialize("AvatarExecutionStrategyImplementation", avatarExecutionStrategy);
+
+        (address compVotingStrategy, ) = noRedeploy(type(CompVotingStrategy).creationCode);
+        json.serialize("CompVotingStrategy", compVotingStrategy);
+
+        (address vanillaAuthenticator, ) = noRedeploy(type(VanillaAuthenticator).creationCode);
+        json.serialize("VanillaAuthenticator", vanillaAuthenticator);
+
+        (address ethTxAuthenticator, ) = noRedeploy(type(EthTxAuthenticator).creationCode);
+        json.serialize("EthTxAuthenticator", ethTxAuthenticator);
+
+        (address ethSigAuthenticator, ) = noRedeploy(
+            abi.encodePacked(type(EthSigAuthenticator).creationCode, abi.encode(name, version))
+        );
+        json.serialize("EthSigAuthenticator", ethSigAuthenticator);
+
+        (address vanillaVotingStrategy, ) = noRedeploy(type(VanillaVotingStrategy).creationCode);
+        json.serialize("VanillaVotingStrategy", vanillaVotingStrategy);
+
+        (address whitelistStrategy, ) = noRedeploy(type(WhitelistStrategy).creationCode);
+        json.serialize("WhitelistStrategy", whitelistStrategy);
+
+        (address proxyFactory, ) = noRedeploy(type(ProxyFactory).creationCode);
+        json.serialize("ProxyFactory", proxyFactory);
+
+        (address space, ) = noRedeploy(abi.encodePacked(type(Space).creationCode, abi.encode(name, version, chainId)));
+        // TODO: initialize space implementation
+        json = json.serialize("SpaceImplementation", space);
+
+        json.write(deploymentsPath);
+
         vm.stopBroadcast();
     }
 
@@ -53,30 +90,20 @@ contract Deployer is Script {
         return abi.decode(vm.parseJson(json, string.concat(network, ".id")), (uint32));
     }
 
-    // Prevents redeployment to a chain
-    function noRedeploy(bytes memory _initCode, string memory contractName) internal returns (address, bool) {
-        // check if the contract name is in deployments
-        try vm.parseJson(deployments, contractName) returns (bytes memory deployedTo) {
-            if (deployedTo.length > 0) {
-                address someContract = abi.decode(deployedTo, (address));
-                // some networks are ephemeral so we need to actually confirm it was deployed
-                // by checking if code is nonzero
-                if (someContract.code.length > 0) {
-                    vm.label(someContract, contractName);
-                    // we already have it deployed
-                    return (someContract, false);
-                }
-            }
-        } catch {}
-        vm.startBroadcast(deployer);
-        address addy = singletonFactory.deploy(_initCode, salt);
-        vm.stopBroadcast();
-        labelAndRecord(addy, contractName);
-        return (addy, true);
+    // Deploys contract if it doesn't exist, otherwise returns the create2 address
+    function noRedeploy(bytes memory _initCode) internal returns (address, bool) {
+        address addy = computeCreate2Address(salt, keccak256(_initCode), address(singletonFactory));
+        if (addy.code.length == 0) {
+            address _addy = singletonFactory.deploy(_initCode, salt);
+            assert(_addy == addy);
+            return (addy, true);
+        } else {
+            return (addy, false);
+        }
     }
 
     function labelAndRecord(address addy, string memory contractName) internal {
         vm.label(addy, contractName);
-        vm.writeFile(deploymentsPath, vm.serializeAddress(deployments, contractName, addy));
+        vm.writeJson(vm.serializeAddress(deployments, contractName, addy), deploymentsPath);
     }
 }
--- test/utils/Space.t.sol
+++ test/utils/Space.t.sol
@@ -5,7 +5,6 @@ pragma solidity ^0.8.18;
 import { Test } from "forge-std/Test.sol";
 import { GasSnapshot } from "forge-gas-snapshot/GasSnapshot.sol";
 import { ERC1967Proxy } from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-
 import { Space } from "../../src/Space.sol";
 import { VanillaAuthenticator } from "../../src/authenticators/VanillaAuthenticator.sol";
 import { VanillaVotingStrategy } from "../../src/voting-strategies/VanillaVotingStrategy.sol";
