6ec8aca5a438fd2e952c0d2e8ab6c2db037743ac
Orlando <orlando@snapshot.org>
2023-03-28 18:21:25 +0100

    chore: added voting power proposal validation strategy back

--- test/VotingPowerProposalValidationStrategy.t.sol
+++ test/VotingPowerProposalValidationStrategy.t.sol
@@ -5,103 +5,109 @@ import { SpaceTest } from "./utils/Space.t.sol";
 import { Strategy, IndexedStrategy } from "../src/types.sol";
 import { CompVotingStrategy } from "../src/voting-strategies/CompVotingStrategy.sol";
 import { VanillaVotingStrategy } from "../src/voting-strategies/VanillaVotingStrategy.sol";
-// import {
-//     VotingPowerProposalValidationStrategy
-// } from "../src/proposal-validation-strategies/VotingPowerProposalValidationStrategy.sol";
+import {
+    VotingPowerProposalValidationStrategy
+} from "../src/proposal-validation-strategies/VotingPowerProposalValidationStrategy.sol";
 import { CompToken } from "./mocks/CompToken.sol";
 
-// contract VotingPowerProposalValidationStrategyTest is SpaceTest {
-//     error DuplicateFound(uint8 index);
-//     uint256 proposalThreshold = 100;
-//     CompToken compToken;
-//     IndexedStrategy[] userPropositionPowerStrategies;
+contract VotingPowerProposalValidationStrategyTest is SpaceTest {
+    error DuplicateFound(uint8 index);
+    uint256 proposalThreshold = 100;
+    CompToken compToken;
+    IndexedStrategy[] userPropositionPowerStrategies;
 
-//     function setUp() public virtual override {
-//         super.setUp();
+    function setUp() public virtual override {
+        super.setUp();
 
-//         compToken = new CompToken();
-//         compToken.mint(author, 100);
-//         vm.prank(author);
-//         compToken.delegate(author);
-//         vm.roll(block.number + 1);
+        compToken = new CompToken();
+        compToken.mint(author, 100);
+        vm.prank(author);
+        compToken.delegate(author);
+        vm.roll(block.number + 1);
 
-//         Strategy memory compVotingStrategy = Strategy(
-//             address(new CompVotingStrategy()),
-//             abi.encodePacked(address(compToken))
-//         );
-//         Strategy[] memory propositionPowerStrategies = new Strategy[](1);
-//         propositionPowerStrategies[0] = compVotingStrategy;
+        // We use a comp token strategy so that we can vary voting power easily and test the proposal threshold
+        Strategy memory compVotingStrategy = Strategy(
+            address(new CompVotingStrategy()),
+            abi.encodePacked(address(compToken))
+        );
+        Strategy[] memory powerStrategies = new Strategy[](1);
+        powerStrategies[0] = compVotingStrategy;
+        string[] memory metadataURIs = new string[](1);
+        space.addVotingStrategies(powerStrategies, metadataURIs);
 
-//         Strategy memory votingPowerProposalValidationStrategy = Strategy(
-//             address(new VotingPowerProposalValidationStrategy()),
-//             abi.encode(proposalThreshold, propositionPowerStrategies)
-//         );
-//         space.setProposalValidationStrategy(votingPowerProposalValidationStrategy);
+        // The comp token strategy will reside at index 1, so we need to use and allowed strategy bit array of ...00010 = 2
+        Strategy memory votingPowerProposalValidationStrategy = Strategy(
+            address(new VotingPowerProposalValidationStrategy()),
+            abi.encode(proposalThreshold, 2)
+        );
+        space.setProposalValidationStrategy(votingPowerProposalValidationStrategy);
 
-//         // The Comp token strategy is at index 0 of the proposal validation strategies
-//         userPropositionPowerStrategies.push(IndexedStrategy(0, new bytes(0)));
-//     }
+        // The Comp token strategy is at index 1 of the proposal validation strategies
+        userPropositionPowerStrategies.push(IndexedStrategy(1, new bytes(0)));
+    }
 
-//     function testPropose() public {
-//         _createProposal(author, proposalMetadataURI, executionStrategy, abi.encode(userPropositionPowerStrategies));
-//     }
+    function testPropose() public {
+        _createProposal(author, proposalMetadataURI, executionStrategy, abi.encode(userPropositionPowerStrategies));
+    }
 
-//     function testProposeInsufficientVotingPower() public {
-//         vm.prank(author);
-//         compToken.burn(author, 1); // Voting power of thr author is  now 99 when the proposal threshold is 100
-//         vm.roll(block.number + 1);
-//         vm.expectRevert(FailedToPassProposalValidation.selector);
-//         _createProposal(author, proposalMetadataURI, executionStrategy, abi.encode(userPropositionPowerStrategies));
-//     }
+    function testProposeInsufficientVotingPower() public {
+        vm.prank(author);
+        compToken.burn(author, 1); // Voting power of thr author is  now 99 when the proposal threshold is 100
+        vm.roll(block.number + 1);
+        vm.expectRevert(FailedToPassProposalValidation.selector);
+        _createProposal(author, proposalMetadataURI, executionStrategy, abi.encode(userPropositionPowerStrategies));
+    }
 
-//     function testProposeInvalidUserVotingStrategy() public {
-//         IndexedStrategy[] memory invalidUserPropositionPowerStrategies = new IndexedStrategy[](1);
-//         invalidUserPropositionPowerStrategies[0] = IndexedStrategy(42, new bytes(0));
+    function testProposeInvalidUserVotingStrategy() public {
+        IndexedStrategy[] memory invalidUserPropositionPowerStrategies = new IndexedStrategy[](1);
+        invalidUserPropositionPowerStrategies[0] = IndexedStrategy(42, new bytes(0));
 
-//         // out of bounds revert
-//         vm.expectRevert();
-//         _createProposal(
-//             author,
-//             proposalMetadataURI,
-//             executionStrategy,
-//             abi.encode(invalidUserPropositionPowerStrategies)
-//         );
-//     }
+        // out of bounds revert
+        vm.expectRevert();
+        _createProposal(
+            author,
+            proposalMetadataURI,
+            executionStrategy,
+            abi.encode(invalidUserPropositionPowerStrategies)
+        );
+    }
 
-//     function testProposeDuplicateUserVotingStrategy() public {
-//         IndexedStrategy[] memory invalidUserPropositionPowerStrategies = new IndexedStrategy[](4);
-//         invalidUserPropositionPowerStrategies[0] = IndexedStrategy(0, new bytes(0));
-//         invalidUserPropositionPowerStrategies[1] = IndexedStrategy(1, new bytes(0));
-//         invalidUserPropositionPowerStrategies[2] = IndexedStrategy(2, new bytes(0));
-//         invalidUserPropositionPowerStrategies[3] = IndexedStrategy(0, new bytes(0)); // Duplicate index
+    function testProposeDuplicateUserVotingStrategy() public {
+        IndexedStrategy[] memory invalidUserPropositionPowerStrategies = new IndexedStrategy[](4);
+        invalidUserPropositionPowerStrategies[0] = IndexedStrategy(0, new bytes(0));
+        invalidUserPropositionPowerStrategies[1] = IndexedStrategy(1, new bytes(0));
+        invalidUserPropositionPowerStrategies[2] = IndexedStrategy(2, new bytes(0));
+        invalidUserPropositionPowerStrategies[3] = IndexedStrategy(0, new bytes(0)); // Duplicate index
 
-//         vm.expectRevert(abi.encodeWithSelector(DuplicateFound.selector, 0));
-//         _createProposal(
-//             author,
-//             proposalMetadataURI,
-//             executionStrategy,
-//             abi.encode(invalidUserPropositionPowerStrategies)
-//         );
-//     }
+        vm.expectRevert(abi.encodeWithSelector(DuplicateFound.selector, 0));
+        _createProposal(
+            author,
+            proposalMetadataURI,
+            executionStrategy,
+            abi.encode(invalidUserPropositionPowerStrategies)
+        );
+    }
 
-//     function testProposeMultipleVotingStrategies() public {
-//         // Using 2 vanilla strategies for proposition power
-//         VanillaVotingStrategy additionalStrategy = new VanillaVotingStrategy();
-//         Strategy[] memory propositionPowerStrategies = new Strategy[](2);
-//         propositionPowerStrategies[0] = Strategy(address(additionalStrategy), new bytes(0));
-//         propositionPowerStrategies[1] = Strategy(address(additionalStrategy), new bytes(0));
+    function testProposeMultipleVotingStrategies() public {
+        // Using 2 vanilla strategies for proposition power
+        VanillaVotingStrategy additionalStrategy = new VanillaVotingStrategy();
+        Strategy[] memory propositionPowerStrategies = new Strategy[](2);
+        propositionPowerStrategies[0] = Strategy(address(additionalStrategy), new bytes(0));
+        propositionPowerStrategies[1] = Strategy(address(additionalStrategy), new bytes(0));
+        string[] memory propositionPowerMetadataURIs = new string[](2);
+        space.addVotingStrategies(propositionPowerStrategies, propositionPowerMetadataURIs);
 
-//         // Using a proposal threshold of 2
-//         Strategy memory votingPowerProposalValidationStrategy = Strategy(
-//             address(new VotingPowerProposalValidationStrategy()),
-//             abi.encode(2, propositionPowerStrategies)
-//         );
-//         space.setProposalValidationStrategy(votingPowerProposalValidationStrategy);
+        // Using a proposal threshold of 2, ...001100 = 12
+        Strategy memory votingPowerProposalValidationStrategy = Strategy(
+            address(new VotingPowerProposalValidationStrategy()),
+            abi.encode(2, 12)
+        );
+        space.setProposalValidationStrategy(votingPowerProposalValidationStrategy);
 
-//         IndexedStrategy[] memory newUserPropositionPowerStrategies = new IndexedStrategy[](2);
-//         newUserPropositionPowerStrategies[0] = IndexedStrategy(0, new bytes(0));
-//         newUserPropositionPowerStrategies[1] = IndexedStrategy(1, new bytes(0));
+        IndexedStrategy[] memory newUserPropositionPowerStrategies = new IndexedStrategy[](2);
+        newUserPropositionPowerStrategies[0] = IndexedStrategy(2, new bytes(0));
+        newUserPropositionPowerStrategies[1] = IndexedStrategy(3, new bytes(0));
 
-//         _createProposal(author, proposalMetadataURI, executionStrategy, abi.encode(newUserPropositionPowerStrategies));
-//     }
-// }
+        _createProposal(author, proposalMetadataURI, executionStrategy, abi.encode(newUserPropositionPowerStrategies));
+    }
+}
