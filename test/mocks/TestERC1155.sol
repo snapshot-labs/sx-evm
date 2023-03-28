6b6f240b7259c8c784319228b522014a4c6f05a5
arr00 <13561405+arr00@users.noreply.github.com>
2023-03-28 11:03:14 -0400

    solhint ignore empty blocks

--- test/mocks/TestERC1155.sol
+++ test/mocks/TestERC1155.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.18;
 import { ERC1155 } from "openzeppelin-contracts/contracts/token/ERC1155/ERC1155.sol";
 
 contract TestERC1155 is ERC1155 {
+    // solhint-disable-next-line no-empty-blocks
     constructor() ERC1155("") {}
 
     function mint(address account, uint256 id, uint256 amount) public {
--- test/mocks/TestERC721.sol
+++ test/mocks/TestERC721.sol
@@ -5,6 +5,7 @@ pragma solidity ^0.8.18;
 import { ERC721 } from "openzeppelin-contracts/contracts/token/ERC721/ERC721.sol";
 
 contract TestERC721 is ERC721 {
+    // solhint-disable-next-line no-empty-blocks
     constructor() ERC721("TEST_ERC721", "TEST_ERC721") {}
 
     function mint(address account, uint256 id) public {
