ce6bce59e59a4b983c700218558fe4235baf7d17
Orlando <orlando@snapshot.org>
2023-05-02 12:19:38 +0100

    chore: natspec on interfaces

--- src/interfaces/ISpace.sol
+++ src/interfaces/ISpace.sol
@@ -8,6 +8,7 @@ import { ISpaceOwnerActions } from "./space/ISpaceOwnerActions.sol";
 import { ISpaceEvents } from "./space/ISpaceEvents.sol";
 import { ISpaceErrors } from "./space/ISpaceErrors.sol";
 
+/// @title Space Interface
 // solhint-disable-next-line no-empty-blocks
 interface ISpace is ISpaceState, ISpaceActions, ISpaceOwnerActions, ISpaceEvents, ISpaceErrors {
 
--- src/interfaces/IVotingStrategy.sol
+++ src/interfaces/IVotingStrategy.sol
@@ -2,17 +2,17 @@
 
 pragma solidity ^0.8.18;
 
-/// @title The interface for voting strategies
+/// @title Voting Strategy Interface
 interface IVotingStrategy {
-    /// @notice Get the voting power of an address at a given timestamp
-    /// @param timestamp The snapshot timestamp to get the voting power at
-    /// If a particular voting strategy requires a  block number instead of a timestamp,
-    /// the strategy should resolve the timestamp to a block number.
-    /// @param voter The address to get the voting power of
-    /// @param params The global parameters that can configure the voting strategy for a particular space
-    /// @param userParams The user parameters that can be used in the voting strategy computation
-    /// @return votingPower The voting power of the address at the given timestamp
-    /// If there is no voting power, return 0.
+    /// @notice Gets the voting power of an address at a given timestamp
+    /// @param timestamp The snapshot timestamp to get the voting power at. If a particular voting strategy
+    ///                  requires a block number instead of a timestamp, the strategy should resolve the
+    ///                  timestamp to a block number.
+    /// @param voter The address to get the voting power of.
+    /// @param params The global parameters that can configure the voting strategy for a particular Space.
+    /// @param userParams The user parameters that can be used in the voting strategy computation.
+    /// @return votingPower The voting power of the address at the given timestamp. If there is no voting power,
+    ///                     return 0.
     function getVotingPower(
         uint32 timestamp,
         address voter,
