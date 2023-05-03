60e554669b76a86ada2cd15de05f0fa98ae79b68
Orlando <orlando@snapshot.org>
2023-05-03 15:01:29 +0100

    refactor: use onlyAuthenticator modifier

--- src/Space.sol
+++ src/Space.sol
@@ -176,11 +176,10 @@ contract Space is ISpace, Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
         // TODO: should we check that there are still authenticators left? same for other setters..
     }
 
-    /**
-     * @notice  Internal function to ensure `msg.sender` is in the list of allowed authenticators.
-     */
-    function _assertValidAuthenticator() internal view {
+    /// @dev Gates access to whitelisted authenticators only.
+    modifier onlyAuthenticator() {
         if (authenticators[msg.sender] != true) revert AuthenticatorNotWhitelisted(msg.sender);
+        _;
     }
 
     /**
@@ -323,9 +322,7 @@ contract Space is ISpace, Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
         string calldata metadataURI,
         Strategy calldata executionStrategy,
         bytes calldata userProposalValidationParams
-    ) external override {
-        _assertValidAuthenticator();
-
+    ) external override onlyAuthenticator {
         // Casting to `uint32` is fine because this gives us until year ~2106.
         uint32 snapshotTimestamp = uint32(block.timestamp);
 
@@ -376,8 +373,7 @@ contract Space is ISpace, Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
         Choice choice,
         IndexedStrategy[] calldata userVotingStrategies,
         string calldata metadataUri
-    ) external override {
-        _assertValidAuthenticator();
+    ) external override onlyAuthenticator {
         Proposal memory proposal = proposals[proposalId];
         _assertProposalExists(proposal);
         if (block.timestamp >= proposal.maxEndTimestamp) revert VotingPeriodHasEnded();
@@ -451,9 +447,7 @@ contract Space is ISpace, Initializable, UUPSUpgradeable, OwnableUpgradeable, Re
         uint256 proposalId,
         Strategy calldata executionStrategy,
         string calldata metadataURI
-    ) external override {
-        _assertValidAuthenticator();
-
+    ) external override onlyAuthenticator {
         Proposal storage proposal = proposals[proposalId];
         if (author != proposal.author) revert InvalidCaller();
         if (block.timestamp >= proposal.startTimestamp) revert VotingDelayHasPassed();
