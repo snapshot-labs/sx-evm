9a4640f8dbd3c6b3aaf1beaed17cbcceda824f8d
pscott <30843220+pscott@users.noreply.github.com>
2023-05-19 16:59:36 +0200

    feat: add emergency execution (#104)
    * feat: add emergency execution
    
    * refactor: emergencyQuorum has priority over quorum; return  when emergency quorum is rejected but still under maxEndTimestamp
    
    * refactor: move len checks to top of initializer
    
    * feat: check array lengths match
    
    * chore: updated tests
    
    * chore: increase coverage
    
    * chore: increase coverage
    
    ---------
    
    Co-authored-by: Orland0x <37511817+Orland0x@users.noreply.github.com>
    Co-authored-by: Orlando <orlando@snapshot.org>

---
+++ src/execution-strategies/EmergencyQuorumStrategy.sol
@@ -0,0 +1,97 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import { IExecutionStrategy } from "../interfaces/IExecutionStrategy.sol";
+import { FinalizationStatus, Proposal, ProposalStatus } from "../types.sol";
+
+abstract contract EmergencyQuorumStrategy is IExecutionStrategy {
+    uint256 public immutable quorum;
+    uint256 public immutable emergencyQuorum;
+
+    constructor(uint256 _quorum, uint256 _emergencyQuorum) {
+        quorum = _quorum;
+        emergencyQuorum = _emergencyQuorum;
+    }
+
+    function execute(
+        Proposal memory proposal,
+        uint256 votesFor,
+        uint256 votesAgainst,
+        uint256 votesAbstain,
+        bytes memory payload
+    ) external virtual override;
+
+    // solhint-disable-next-line code-complexity
+    function getProposalStatus(
+        Proposal memory proposal,
+        uint256 votesFor,
+        uint256 votesAgainst,
+        uint256 votesAbstain
+    ) public view override returns (ProposalStatus) {
+        bool emergencyQuorumReached = _quorumReached(emergencyQuorum, votesFor, votesAgainst, votesAbstain);
+
+        bool accepted = _quorumReached(quorum, votesFor, votesAgainst, votesAbstain) &&
+            _supported(votesFor, votesAgainst);
+
+        if (proposal.finalizationStatus == FinalizationStatus.Cancelled) {
+            return ProposalStatus.Cancelled;
+        } else if (proposal.finalizationStatus == FinalizationStatus.Executed) {
+            return ProposalStatus.Executed;
+        } else if (block.timestamp < proposal.startTimestamp) {
+            return ProposalStatus.VotingDelay;
+        } else if (emergencyQuorumReached) {
+            if (_supported(votesFor, votesAgainst)) {
+                // Proposal is supported
+                if (block.timestamp < proposal.maxEndTimestamp) {
+                    // New votes can still come in so return `VotingPeriodAccepted`.
+                    return ProposalStatus.VotingPeriodAccepted;
+                } else {
+                    // No new votes can't come in, so it's definitely accepted.
+                    return ProposalStatus.Accepted;
+                }
+            } else {
+                // Proposal is not supported
+                if (block.timestamp < proposal.maxEndTimestamp) {
+                    // New votes might still come in so return `VotingPeriod`.
+                    return ProposalStatus.VotingPeriod;
+                } else {
+                    // New votes can't come in, so it's definitely rejected.
+                    return ProposalStatus.Rejected;
+                }
+            }
+        } else if (block.timestamp < proposal.minEndTimestamp) {
+            // Proposal has not reached minEndTimestamp yet.
+            return ProposalStatus.VotingPeriod;
+        } else if (block.timestamp < proposal.maxEndTimestamp) {
+            // Timestamp is between minEndTimestamp and maxEndTimestamp
+            if (accepted) {
+                return ProposalStatus.VotingPeriodAccepted;
+            } else {
+                return ProposalStatus.VotingPeriod;
+            }
+        } else if (accepted) {
+            // Quorum reached and proposal supported: no new votes will come in so the proposal is
+            // definitely  accepted.
+            return ProposalStatus.Accepted;
+        } else {
+            // Quorum not reached reached or proposal supported: no new votes will come in so the proposal is
+            // definitely rejected.
+            return ProposalStatus.Rejected;
+        }
+    }
+
+    function _quorumReached(
+        uint256 _quorum,
+        uint256 _votesFor,
+        uint256 _votesAgainst,
+        uint256 _votesAbstain
+    ) internal pure returns (bool) {
+        uint256 totalVotes = _votesFor + _votesAgainst + _votesAbstain;
+        return totalVotes >= _quorum;
+    }
+
+    function _supported(uint256 _votesFor, uint256 _votesAgainst) internal pure returns (bool) {
+        return _votesFor > _votesAgainst;
+    }
+}
