c73eb719f185bf610212c2572dfe3ddc20d89fed
pscott <30843220+pscott@users.noreply.github.com>
2023-06-14 14:25:39 +0200

    audit: remove binary search in whitelist strategy (#200)

--- test/WhitelistVotingStrategy.t.sol
+++ test/WhitelistVotingStrategy.t.sol
@@ -5,32 +5,25 @@ import { Test } from "forge-std/Test.sol";
 import { WhitelistVotingStrategy } from "../src/voting-strategies/WhitelistVotingStrategy.sol";
 
 contract WhitelistVotingStrategyTest is Test {
+    error VoterAndIndexMismatch();
+
     WhitelistVotingStrategy public whitelistVotingStrategy;
 
     function testWhitelistVotingPower() public {
         WhitelistVotingStrategy.Member[] memory members = new WhitelistVotingStrategy.Member[](3);
-        members[0] = WhitelistVotingStrategy.Member(address(1), 11);
-        members[1] = WhitelistVotingStrategy.Member(address(3), 33);
+        members[0] = WhitelistVotingStrategy.Member(address(3), 33);
+        members[1] = WhitelistVotingStrategy.Member(address(1), 11);
         members[2] = WhitelistVotingStrategy.Member(address(5), 55);
         whitelistVotingStrategy = new WhitelistVotingStrategy();
 
         bytes memory params = abi.encode(members);
 
-        assertEq(whitelistVotingStrategy.getVotingPower(0, members[0].addr, params, ""), members[0].vp);
-        assertEq(whitelistVotingStrategy.getVotingPower(0, members[1].addr, params, ""), members[1].vp);
-        assertEq(whitelistVotingStrategy.getVotingPower(0, members[2].addr, params, ""), members[2].vp);
-
-        // Index 0
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(0), params, ""), 0);
-        // Index 2
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(2), params, ""), 0);
-        // 4
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(4), params, ""), 0);
-        // Last index
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(6), params, ""), 0);
+        assertEq(whitelistVotingStrategy.getVotingPower(0, members[0].addr, params, abi.encode(0)), members[0].vp);
+        assertEq(whitelistVotingStrategy.getVotingPower(0, members[1].addr, params, abi.encode(1)), members[1].vp);
+        assertEq(whitelistVotingStrategy.getVotingPower(0, members[2].addr, params, abi.encode(2)), members[2].vp);
     }
 
-    function testWhitelistVotingPowerSmall() public {
+    function testWhitelistVoterAndIndexMismatch() public {
         WhitelistVotingStrategy.Member[] memory members = new WhitelistVotingStrategy.Member[](3);
         members[0] = WhitelistVotingStrategy.Member(address(1), 11);
         members[1] = WhitelistVotingStrategy.Member(address(3), 33);
@@ -38,14 +31,8 @@ contract WhitelistVotingStrategyTest is Test {
 
         bytes memory params = abi.encode(members);
 
-        assertEq(whitelistVotingStrategy.getVotingPower(0, members[0].addr, params, ""), members[0].vp);
-        assertEq(whitelistVotingStrategy.getVotingPower(0, members[1].addr, params, ""), members[1].vp);
-
-        // Index 0
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(0), params, ""), 0);
-        // Index 2
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(2), params, ""), 0);
-        // Last index
-        assertEq(whitelistVotingStrategy.getVotingPower(0, address(4), params, ""), 0);
+        vm.expectRevert(VoterAndIndexMismatch.selector);
+        // `voter` is members[0] but the `voterIndex` is 1 (which corresponds to members[1]).
+        whitelistVotingStrategy.getVotingPower(0, members[0].addr, params, abi.encode(1));
     }
 }
