47d612bddcf25d260def59d3c813da579799e93a
Orlando <orlando@snapshot.org>
2023-06-20 15:18:27 +0100

    fix: update finalized propsal test

---
+++ test/UpdateProposal.t.sol
@@ -0,0 +1,98 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import { SpaceTest } from "./utils/Space.t.sol";
+import { VanillaExecutionStrategy } from "../src/execution-strategies/VanillaExecutionStrategy.sol";
+import { Choice, IndexedStrategy, Strategy, UpdateSettingsCalldata } from "../src/types.sol";
+
+contract UpdateProposalTest is SpaceTest {
+    string internal newMetadataURI = "Testing123";
+    Strategy internal newStrategy;
+
+    function setUp() public virtual override {
+        super.setUp();
+
+        newStrategy = Strategy(address(new VanillaExecutionStrategy(owner, quorum)), new bytes(0));
+
+        // Set the votingDelay to 10.
+        votingDelay = 10;
+        space.updateSettings(
+            UpdateSettingsCalldata(
+                NO_UPDATE_UINT32,
+                NO_UPDATE_UINT32,
+                votingDelay,
+                NO_UPDATE_STRING,
+                NO_UPDATE_STRING,
+                NO_UPDATE_STRATEGY,
+                NO_UPDATE_STRING,
+                NO_UPDATE_ADDRESSES,
+                NO_UPDATE_ADDRESSES,
+                NO_UPDATE_STRATEGIES,
+                NO_UPDATE_STRINGS,
+                NO_UPDATE_UINT8S
+            )
+        );
+    }
+
+    function _updateProposal(
+        address _author,
+        uint256 _proposalId,
+        Strategy memory _executionStrategy,
+        string memory _metadataURI
+    ) public {
+        vanillaAuthenticator.authenticate(
+            address(space),
+            UPDATE_PROPOSAL_SELECTOR,
+            abi.encode(_author, _proposalId, _executionStrategy, _metadataURI)
+        );
+    }
+
+    function testUpdateProposal() public {
+        uint256 proposalId = _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+
+        vm.expectEmit(true, true, true, true);
+        emit ProposalUpdated(proposalId, newStrategy, newMetadataURI);
+
+        // Update metadata.
+        _updateProposal(author, proposalId, newStrategy, newMetadataURI);
+
+        // Fast forward and finish the proposal to ensure everything is still working properly.
+        vm.roll(block.number + votingDelay);
+        _vote(author, proposalId, Choice.For, userVotingStrategies, voteMetadataURI);
+        space.execute(proposalId, executionStrategy.params);
+    }
+
+    function testUpdateFinalizedProposal() public {
+        uint256 proposalId = _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+        vm.roll(block.number + votingDelay);
+        _vote(author, proposalId, Choice.For, userVotingStrategies, voteMetadataURI);
+        space.execute(proposalId, executionStrategy.params);
+
+        vm.expectRevert(abi.encodeWithSelector(ProposalFinalized.selector));
+        _updateProposal(author, proposalId, newStrategy, newMetadataURI);
+    }
+
+    function testUpdateProposalAfterDelay() public {
+        uint256 proposalId = _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+        vm.roll(block.number + votingDelay);
+
+        vm.expectRevert(VotingDelayHasPassed.selector);
+        // Try to update metadata. Should fail.
+        _updateProposal(author, proposalId, newStrategy, newMetadataURI);
+    }
+
+    function testUpdateProposalInvalidCaller() public {
+        uint256 proposalId = _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+
+        vm.expectRevert(InvalidCaller.selector);
+        _updateProposal(address(42), proposalId, newStrategy, newMetadataURI);
+    }
+
+    function testUpdateProposalUnauthenticated() public {
+        uint256 proposalId = _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+
+        vm.expectRevert(abi.encodeWithSelector(AuthenticatorNotWhitelisted.selector));
+        space.updateProposal(author, proposalId, newStrategy, newMetadataURI);
+    }
+}
