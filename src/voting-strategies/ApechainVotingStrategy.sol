1f3ca8b46daca0b5a8e592f702ab06e4eafa4bb8
Scott Piriou <30843220+pscott@users.noreply.github.com>
2025-05-07 14:19:47 +0200

    feat: placeholder apechain voting

---
+++ src/voting-strategies/ApechainVotingStrategy.sol
@@ -0,0 +1,49 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import { IVotingStrategy } from "../interfaces/IVotingStrategy.sol";
+
+struct PackedTrieNode {
+    uint256 data1;
+    uint256 data2;
+    uint256 data3;
+}
+
+struct VotingTrieParameters {
+    bytes accountProof;
+    address account;
+    bool committmentHasDelegated;
+    uint256 committmentVotingPower;
+    PackedTrieNode[] nodes;
+}
+
+interface IApeChainVotingPower {
+    function computeVotingPower(
+        VotingTrieParameters calldata trieParams,
+        bytes32 proposalId,
+        uint256 blockNumber
+    ) external view returns (uint256);
+}
+
+/// @title Vanilla Voting Strategy
+contract ApechainVotingStrategy is IVotingStrategy {
+    function getVotingPower(
+        uint32 blockNumber,
+        address voter,
+        bytes calldata params, // (address herodotusContract)
+        bytes calldata userParams // (VotingTrieParameters votingTrieParameters, uint256 proposalId) TODO check proposalId
+    ) external view override returns (uint256) {
+        // Decode the parameters
+        address contractAddress = abi.decode(params, (address));
+        (VotingTrieParameters memory votingTrieParameters, bytes32 proposalId) = abi.decode(
+            userParams,
+            (VotingTrieParameters, bytes32)
+        );
+        // Get the contract instance
+        IApeChainVotingPower herodotusContract = IApeChainVotingPower(contractAddress);
+
+        // Call the computeVotingPower function
+        return herodotusContract.computeVotingPower(votingTrieParameters, proposalId, blockNumber);
+    }
+}
