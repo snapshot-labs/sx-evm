29847a8ffb9a82e18c587daddd812e1a8ca6ed16
Scott Piriou <30843220+pscott@users.noreply.github.com>
2025-05-29 13:39:38 +0200

    feat: current gas proposal validation strategy

---
+++ src/proposal-validation-strategies/CurrentGasProposalValidationStrategy.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity ^0.8.18;
+
+import { IProposalValidationStrategy } from "../interfaces/IProposalValidationStrategy.sol";
+
+/// @title Current Gas Proposal Validation Strategy
+contract CurrentGasProposalValidationStrategy is IProposalValidationStrategy {
+    function validate(
+        address author,
+        bytes calldata params, // (uint256 threshold)
+        bytes calldata // userParams
+    ) external view override returns (bool) {
+        // Decode threshold from params
+        uint256 threshold = abi.decode(params, (uint256));
+
+        // Ensure author has a balance greater than threshold
+        return author.balance >= threshold;
+    }
+}
---
+++ test/CurrentGasProposalValidationStrategy.t.sol
@@ -0,0 +1,56 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.18;
+
+import { SpaceTest } from "./utils/Space.t.sol";
+import { Strategy, UpdateSettingsCalldata } from "../src/types.sol";
+import {
+    CurrentGasProposalValidationStrategy
+} from "../src/proposal-validation-strategies/CurrentGasProposalValidationStrategy.sol";
+
+contract CurrentGasTest is SpaceTest {
+    CurrentGasProposalValidationStrategy internal currentGasProposalValidationStrategy;
+    uint256 threshold = 1 ether; // minimum of 1 ETH
+
+    function setUp() public override {
+        super.setUp();
+
+        currentGasProposalValidationStrategy = new CurrentGasProposalValidationStrategy();
+
+        Strategy memory newProposalStrategy = Strategy(
+            address(currentGasProposalValidationStrategy),
+            abi.encode(threshold)
+        );
+        space.updateSettings(
+            UpdateSettingsCalldata(
+                NO_UPDATE_UINT32,
+                NO_UPDATE_UINT32,
+                NO_UPDATE_UINT32,
+                NO_UPDATE_STRING,
+                NO_UPDATE_STRING,
+                newProposalStrategy,
+                "",
+                NO_UPDATE_ADDRESSES,
+                NO_UPDATE_ADDRESSES,
+                NO_UPDATE_STRATEGIES,
+                NO_UPDATE_STRINGS,
+                NO_UPDATE_UINT8S
+            )
+        );
+    }
+
+    function test_hasJustEnoughGas() public {
+        vm.deal(author, threshold);
+        _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+    }
+
+    function test_hasEnoughGas() public {
+        vm.deal(author, threshold + 1);
+        _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+    }
+
+    function test_hasNotEnoughGas() public {
+        vm.deal(author, threshold - 1);
+        vm.expectRevert(abi.encodeWithSelector(FailedToPassProposalValidation.selector));
+        _createProposal(author, proposalMetadataURI, executionStrategy, new bytes(0));
+    }
+}
